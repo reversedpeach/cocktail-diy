a8bb8374e89e48f950c1b15b35678fd67c78ab99
Rongfei Pan <rongfei@kth.se>
2022-10-15 23:13:20 +0200

    merge main

--- client/model/cocktailModel.js
+++ client/model/cocktailModel.js
@@ -1,30 +1,27 @@
 class CocktailModel {
+	constructor() {
+		this.subscribers = [];
+		this.mybar = [
+			"Gin",
+			"Vodka",
+			"Cointreau",
+			"Amaretto",
+			"Rum",
+			"Whiskey",
+			"Champagne",
+			"Bourbon",
+			"Tequila",
+			"Bacardi",
+		]; // array with objects of drinks
+		this.currentdrink = []; // array with ingredients
+		this.drinkdetails = null;
+		this.selectedmode = "discover";
+		this.createddrink = [];
+		this.favoritedrinks = "Pink Moon";
+		this.likeddrinks = ["Abbey Cocktail", "Pink Moon", "Singapore Sling"];
+		this.recentdrinks = ["Lone Tree Cocktail", "Rose", "Tom Collins", "Martini"];
+	}
 
-    constructor() {
-        this.subscribers = [];
-        this.mybar = [
-            "Gin",
-            "Vodka",
-            "Cointreau",
-            "Amaretto",
-            "Rum",
-            "Whiskey",
-            "Champagne",
-            "Bourbon",
-            "Tequila",
-            "Bacardi",
-        ]; // array with objects of drinks
-        this.currentdrink = []; // array with ingredients
-        this.drinkdetails = null;
-        this.selectedmode = "discover";
-        this.createddrink = [];
-        this.favoritedrinks = "Pink Moon";
-        this.likeddrinks = ["Abbey Cocktail", "Pink Moon", "Singapore Sling"];
-        this.recentdrinks = ["Lone Tree Cocktail", "Rose", "Tom Collins", "Martini"];
-    }
-
-
-<<<<<<< HEAD
 	addMyBar(ing) {
 		this.mybar = this.mybar.concat(ing);
 		this.notifyObservers();
@@ -42,69 +39,55 @@ class CocktailModel {
 		console.log(ing + " is added");
 		this.notifyObservers();
 	}
-=======
-    setIngList(ingList) {
-        this.currentdrink = ingList;
-        this.notifyObservers();
-    }
 
-    addIngShaker(ing) {
-        var tempSet = new Set(this.currentdrink.concat(ing));
-        this.currentdrink = Array.from(tempSet);
-        console.log(ing + " is added");
-        this.notifyObservers();
-    }
->>>>>>> 6a89517ff92598dac8d0c1fdc077909fb501a347
+	emptyShaker() {
+		this.currentdrink = [];
+	}
+	setMode() {
+		if (this.selectedmode === "discover") {
+			this.selectedmode = "create";
+		} else {
+			this.selectedmode = "discover";
+		}
+		this.notifyObservers();
+	}
 
-    emptyShaker() {
-        this.currentdrink = [];
-    }
-    setMode() {
-        if (this.selectedmode === "discover") {
-            this.selectedmode = "create";
-        } else {
-            this.selectedmode = "discover";
-        }
-        this.notifyObservers();
-    }
+	setDetails(drink) {
+		this.drinkdetails = drink;
+		this.notifyObservers();
+	}
 
+	getDetailsDrink() {
+		return this.drinkdetails;
+	}
 
-    setDetails(drink) {
-        this.drinkdetails = drink;
-        this.notifyObservers();
-    }
+	removeIngShaker(ing) {
+		this.currentdrink = this.currentdrink.filter((elem) => elem !== ing);
+		this.notifyObservers();
+	}
 
-    getDetailsDrink() {
-        return this.drinkdetails;
-    }
+	getCurrentDrink() {
+		return this.currentdrink;
+	}
 
-    removeIngShaker(ing) {
-        this.currentdrink = this.currentdrink.filter((elem) => elem !== ing);
-        this.notifyObservers();
-    }
+	addObserver(obs) {
+		this.subscribers = this.subscribers.concat(obs);
+		return () => this.removeObserver(obs);
+	}
 
-    getCurrentDrink() {
-        return this.currentdrink;
-    }
-
-    addObserver(obs) {
-        this.subscribers = this.subscribers.concat(obs);
-        return () => this.removeObserver(obs);
-    }
-
-    notifyObservers() {
-        this.subscribers.forEach((callback) => {
-            //console.log(callback);
-            try {
-                callback();
-            } catch (err) {
-                console.error("Error ", err, callback);
-            }
-        });
-    }
-    removeObserver(obs) {
-        this.subscribers = this.subscribers.filter((o) => o !== obs);
-    }
+	notifyObservers() {
+		this.subscribers.forEach((callback) => {
+			//console.log(callback);
+			try {
+				callback();
+			} catch (err) {
+				console.error("Error ", err, callback);
+			}
+		});
+	}
+	removeObserver(obs) {
+		this.subscribers = this.subscribers.filter((o) => o !== obs);
+	}
 }
 
 export default CocktailModel;
