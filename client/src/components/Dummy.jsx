0221d79de733ce706e1547b991ec228c35bc4ca6
mnordste <mnordste@kth.se>
2022-10-18 13:18:21 +0200

    Removed client folder to simplify merging with main

--- client/src/components/Dummy.jsx
+++
@@ -1,9 +0,0 @@
-//Useful if a component is as it should be, tells that it has changed
-
-import React from "react";
-
-export const Dummy = ({shouldRenderText, text}) => {
-
-    return shouldRenderText && <div>{"HAHA"}</div>
-
-}
--- client/src/webAPI/webAPI.ts
+++
@@ -1,3 +0,0 @@
-import axios from "axios";
-
-export const getAPI = () => axios.get("https://localhost:8080/api/getsomedata", {withCredentials:true}).then(({ data }:{data:{ someData:string } }) => data.someData);
